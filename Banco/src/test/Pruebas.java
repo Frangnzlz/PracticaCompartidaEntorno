83a47d5a6f95b4ae5c28ecb1d016afc7cb929c60
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-04-30 17:28:40 +0200

    test

--- Banco/src/test/Pruebas.java
+++ Banco/src/test/Pruebas.java
@@ -1,5 +1,14 @@
 package test;
 
-public class Pruebas {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Pruebas {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 
 }
