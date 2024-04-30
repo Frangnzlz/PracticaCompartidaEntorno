2c64c9bb6be62628116ef2dacb0c34681ea4cac9
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-04-30 17:05:49 +0200

    Clase Cuenta

--- Banco/src/models/Cuenta.java
+++ Banco/src/models/Cuenta.java
@@ -1,5 +1,45 @@
 package models;
 
 public class Cuenta {
-
+	private String iban;
+	private Cliente cliente;
+	private double cantidad;
+	private String estado;
+	
+	/**
+	 * 
+	 * @param iban
+	 * @param cliente
+	 * @param cantidad
+	 */
+	public Cuenta(String iban, Cliente cliente, double cantidad) {
+		this.iban = iban;
+		this.cliente = cliente;
+		this.cantidad = cantidad;
+		estado = "Activa";
+	}
+	
+	/**
+	 * 
+	 * @param cantidad
+	 */
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public double getCantidad() {
+		return cantidad;
+	}
+	/**
+	 * 
+	 * @param estado
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
 }
