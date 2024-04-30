6527e86cfa7b051c74dd402863a0dc2993083926
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-04-30 17:21:47 +0200

    Clase operaciones

--- Banco/src/util/Operaciones.java
+++ Banco/src/util/Operaciones.java
@@ -1,5 +1,36 @@
 package util;
 
+import models.Cuenta;
+
+
 public abstract class Operaciones {
 
+	
+	public static void transferencia(Cuenta cuentaOrigen, Cuenta cuentaDestino, double cantidad) {
+		double cantidadOrigen = cuentaOrigen.getCantidad();
+		double cantidadDestino = cuentaDestino.getCantidad();
+		
+		cantidadOrigen -= cantidad;
+		cantidadDestino += cantidad;
+		
+		cuentaOrigen.setCantidad(cantidadOrigen);
+		cuentaDestino.setCantidad(cantidadDestino);
+		
+	}
+	
+	
+	public static void prestamo(Cuenta cuentaDestino, double cantidad) {
+		double total = cuentaDestino.getCantidad();
+		
+		total += cantidad;
+		
+		cuentaDestino.setCantidad(total);
+		
+	}
+	
+	
+	public static void cancelarCuenta(Cuenta cuentaDestino) {
+		cuentaDestino.setEstado("Cancelada");
+	}
+	
 }
