0b17bf09aabd8c2a0dff571022e96bc98d7d5172
Promerante <facundodanielbenitez@gmail.com>
2024-04-30 17:42:12 +0200

    Test

--- Banco/src/test/Pruebas.java
+++ Banco/src/test/Pruebas.java
@@ -1,8 +1,13 @@
 package test;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
+import models.*;
+import util.*;
 
 import org.junit.jupiter.api.Test;
+
+
 
 class Pruebas {
 
@@ -10,5 +15,18 @@ class Pruebas {
 	void test() {
 		fail("Not yet implemented");
 	}
+	
+	
+	@Test
+	void transferencia(Cuenta cuentaOrigen, Cuenta cuentaDestino, double cantidad) {
+		double esperada=50;
+		Cuenta cuenta1=new Cuenta(null, null, 150);
+		Cuenta cuenta2=new Cuenta(null, null, 150);
+		Operaciones.transferencia(cuenta1, cuenta2, 100);
+		double realizado=cuenta1.getCantidad();
+		assertEquals(esperada, realizado);		
+	}
+
+
 
 }
